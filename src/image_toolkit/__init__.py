cfb2d428c0dcf0579b6d2bf38d8d2589476367f8
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2026-01-28 15:12:37 -0600

    feat: create __init__ file

---
+++ src/image_toolkit/__init__.py
@@ -0,0 +1,11 @@
+"""
+Image Analysis Toolkit
+A library for precise image generation and cryptanalytic analysis.
+"""
+
+__version__ = "0.1.0"
+
+from .generator import ImageGenerator
+from .metrics import ImageMetrics
+
+__all__ = ['ImageGenerator', 'ImageMetrics']
