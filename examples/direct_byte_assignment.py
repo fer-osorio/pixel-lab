46c8d0a7e966af91a9d1a3ed655d37f214cbfb29
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2026-02-25 12:58:52 -0600

    feat: modify image creation for more complex and aesthetic patterns

--- examples/direct_byte_assignment.py
+++ examples/direct_byte_assignment.py
@@ -1,15 +1,31 @@
+from math import cos, sqrt
+
 from pixel_lab import ImageGenerator
+
+
+def func_on_image(x: int):
+    y = x / 16
+    return int(y * sqrt(y) * cos(y / 32))
 
 
 def direct_byte_assignment():
     """Example: Create a pattern using direct byte assignment."""
-    print("\n=== Example 2: Direct Byte Assignment (Incresing function) ===")
+    print("\n=== Example: Direct Byte Assignment ===")
 
-    gen = ImageGenerator(256, 256)
+    gen = ImageGenerator(1152, 512)
 
     # Assigning each byte the value of its index module 256
-    for i in range(gen.byte_count):
-        gen.set_byte(i, i & 255)
+    j = 0
+    k = -1
+    offset = 0
+    for i in range(0, gen.byte_count, 3):
+        j = (i // 3) & 255
+        if j == 0:
+            k += 1
+            offset = func_on_image(k)
+        gen.set_byte(i, (j + offset) & 255)
+        gen.set_byte(i + 1, (j + offset) & 255)
+        gen.set_byte(i + 2, (j + offset) & 255)
 
     gen.save("direct_byte_assignment.png")
 
